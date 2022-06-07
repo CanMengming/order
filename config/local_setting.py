844dc1e9d5508e06dfe95e6f7621c53b107ae5d6
ming <55344053+CanMengming@users.noreply.github.com>
2022-06-07 15:12:07 +0800

    Update local_setting.py

--- config/local_setting.py
+++ config/local_setting.py
@@ -2,7 +2,7 @@ DEBUG = True        # 测试环境下开启debug模式
 SQLALCHEMY_ECHO = True      # 是否打印输出全部SQL语句
 
 # 测试环境下配置数据库
-SQLALCHEMY_DATABASE_URI = 'mysql://root:qq276713@127.0.0.1/food_db'   # 使用Linux本地food_db数据库
+SQLALCHEMY_DATABASE_URI = 'mysql://root:密码@127.0.0.1/food_db'   # 使用Linux本地food_db数据库
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ENCODING = "utf8mb4"        # sql的编码设置为utf-8
 
